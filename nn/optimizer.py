9b319cda413e9b5b11f789ec21d060760161dc25
gilfoyle19 <kartheekc19@gmail.com>
2025-05-10 17:57:20 +0200

    Added the SGD optimizer

--- nn/optimizer.py
+++ nn/optimizer.py
@@ -2,5 +2,34 @@ import numpy as np
 from numpy.typing import NDArray
 
 class Optimizer:
-    pass
+    def __init__(self, learning_rate: float):
+        """
+        Initialize the optimizer with a learning rate.
+        """
+        self._learning_rate = learning_rate 
+        self._layer_number = 0 # Number of layers in the model
 
+        @property
+        def layer_number(self):
+            """
+            Returns the number of layers in the model.
+            """
+            return self._layer_number
+        
+        @layer_number.setter
+        def layer_number(self, layer_number: int):
+            """
+            Sets the number of layers in the model.
+            """
+            self._layer_number = layer_number
+
+        def update_weights(self, layer, grad_weights):
+            """
+            Update the weights of the layer using the optimizer.
+            """
+            layer.weights -= self._learning_rate * grad_weights
+        def update_bias(self, layer, grad_bias):
+            """
+            Update the bias of the layer using the optimizer.
+            """
+            layer.bias -= self._learning_rate * grad_bias
